c60281be5aae34fe016482dc5681fe80514bc42f
dk-frkw <sp.daiki98@gmail.com>
2020-11-29 20:45:54 +0900

    商品出品機能の実装

---
+++ app/models/item.rb
@@ -0,0 +1,31 @@
+class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :delivery_area_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+  belongs_to :user
+  has_one    :purchase
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_area
+  belongs_to :delivery_days
+  
+   with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :delivery_area_id
+    validates :delivery_days_id
+  end
+end
